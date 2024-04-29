e47a9ed264fe9377d0030332fddead3222316d4a
Joaobcamargos <129392062+Joaobcamargos@users.noreply.github.com>
2024-04-28 23:58:52 -0300

    Add files via upload

--- Basic/dictionary_hwtu.py
+++ Basic/dictionary_hwtu.py
@@ -6,10 +6,17 @@ Created on Thu Apr 25 15:09:44 2024
 """
 
 keys = ['a','b','c','d','e']
+valores=[5,4,3,2,1]
 dic={}
 for i in range(5):
-    dic[keys[i]]=i;
+    dic[keys[i]]=valores[i];
+
     
+
 print(dic)
 print(dic['b']);
 dic.get('b')
+
+
+val2=list(dic.keys())
+val3=list(dic.values())
