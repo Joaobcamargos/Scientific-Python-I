c1afe77e15b07f0b54bb3f50265e85e2d9aff55f
Joaobcamargos <joaovitorbc3033@gmail.com>
2024-07-27 18:27:46 -0300

    Generalização do código

--- Matplot/Ex1.py
+++ Matplot/Ex1.py
@@ -2,10 +2,12 @@
 #Plotando uma circunfência a partir de 2 funções.
 import matplotlib.pyplot as plt
 import math
-
-x = [x for x in range(201)] # range de 2*raio+1
-y = [-math.sqrt(10000-(y-100)**2)+100 for y in x]
-k = [math.sqrt(10000-(y-100)**2)+100 for y in x]
+r=100
+a=100
+b=100
+x = [x for x in range(2*r+1)] # range de 2*raio+1
+y = [-math.sqrt(r**2-(y-a)**2)+b for y in x]
+k = [math.sqrt(r**2-(y-a)**2)+b for y in x]
 
 
 plt.plot(x, y)
